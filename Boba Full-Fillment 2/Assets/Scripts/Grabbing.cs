4a093d65552d1ccf1ff1d0b4b5f15ec87892e054
Destin Estrela <destin.estrela@gmail.com>
2022-02-21 13:43:07 -0800

    Simple first person movement and grabbing

---
+++ Boba Full-Fillment 2/Assets/Scripts/Grabbing.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Grabbing : MonoBehaviour
+{
+
+    private bool grabObj = false;
+    private RaycastHit hit;
+    GameObject hitObj = null;
+    public Transform grabPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.isPressed)
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 5f, ~(1 << 8)))
+            {
+                if (hit.collider.gameObject && grabObj == false)
+                {
+                    hitObj = hit.collider.gameObject;
+                    hitObj.GetComponent<Rigidbody>().isKinematic = true;
+                    grabObj = true;
+                }
+            }
+            if (grabObj && hitObj != null)
+            {
+                //Moving object with player, 2 units in front of him cause we want to see it.
+                hitObj.transform.position = grabPoint.position;
+            }
+        }
+        else
+        {
+            if(hitObj != null)
+            {
+                hitObj.GetComponent<Rigidbody>().isKinematic = false;
+            }
+            grabObj = false;
+        }
+    }
+}
